4d13daf74075f61f4faac875eb1b0b328ccf8eee
anibalsci <anibalsci@bitbucket.org>
2019-01-09 18:35:01 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-nav tabs>
+        <b-nav-item exact to="/activities">Formulario de actividades</b-nav-item>
+      </b-nav>
+    </b-container>
+    <router-view/>
+  </div>
+</template>
